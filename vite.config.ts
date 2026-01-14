0c209f65870f09367c8e1976f0fe47fb44a4225d
BarylStyle <barylstyle@example.com>
2026-01-14 18:41:26 +0100

    build: dynamic base for GH Pages vs Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/real-size-map/',
+  // Build paths:
+  // - GitHub Pages needs '/real-size-map/'
+  // - Vercel/local should use '/'
+  base: process.env.GITHUB_ACTIONS ? '/real-size-map/' : '/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
